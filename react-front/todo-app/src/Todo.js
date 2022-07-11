7e133d2ed41065da41a43ae00fbe0b62d922ab3b
skyriv213 <gn1007@dankook.ac.kr>
2022-07-12 01:06:57 +0900

    readme 파일 수정 및 front 기본 틀 생성

---
+++ react-front/todo-app/src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Todo extends React.Component{
+    render(){
+        return(
+            <div className="Todo">
+                <input type="checkbox" id="Todo0" name="Todo0" value="Todo0"/>
+                <label for="Todo0">Todo ������Ʈ �����</label>
+            </div>
+        );
+    }
+}
+
+export default Todo;
